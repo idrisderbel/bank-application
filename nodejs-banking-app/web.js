52abcf2d8add902f1fb711a9582bc61a9a2a95ae
Idriss Derbel <idrisderbelpro@gmail.com>
2023-01-31 14:57:42 +0100

    express js integration

---
+++ nodejs-banking-app/web.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+const {createNewAccount,deposit,withdraw,balance, transfer} = require('./db')
+
+const port=3000
+
+app.post('/create', express.json(), (req, res) => {
+    createNewAccount( req.body.acName,req.body.balance , (msg) => {
+        res.json({ 'sts' : 'success', msg })
+    })
+})
+
+app.put('/transfer', express.json() ,(req, res) => {
+    transfer(req.body.srcId,req.body.destId,req.body.amount, msg => {
+        res.json({ 'sts' : 'success', msg })
+    })
+})
+
+app.put('/withdraw', express.json(), (req, res) => {
+    withdraw(req.body.acId,req.body.amount, msg => {
+        res.json({ 'sts' : 'success', msg })
+    })
+})
+
+app.put( '/deposit', express.json() ,(req, res) => {
+    deposit(req.body.acId,req.body.amount, msg => {
+        res.json({ 'sts' : 'success', msg })
+    })
+} )
+
+app.get('/balance/:acId', ( req, res ) => {
+    console.log(req.params)
+    const acId = req.params.acId
+    // balance(acId)
+    balance(acId, bal => {
+        res.json({ bal })
+    })
+})
+
+app.listen(port,() => {
+    console.log(`Banking app listening on port ${port}`)
+})
